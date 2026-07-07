adf465b8419ad3aa53c510d061a6cac771a4b0e5
Prince <ghoshprince999@gmail.com>
2026-07-07 15:49:24 +0600

    setup GetX and GetStorage initialization

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:todo_with_getstorage/screens/home_screen.dart';
 import 'package:todo_with_getstorage/screens/save_screen.dart';
+import 'package:get/get.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   runApp(const MyApp());
 }
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'To Do',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
